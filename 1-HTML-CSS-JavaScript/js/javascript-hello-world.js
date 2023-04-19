9fc6c7bf40bda9295d2ecfbed62dc038eb3ad806
Susi yanti Gultom <susiyantigultom@Susis-MacBook-Air.local>
2023-04-19 11:45:36 +0800

    first commit

---
+++ 1-HTML-CSS-JavaScript/js/javascript-hello-world.js
@@ -0,0 +1,8 @@
+function getname() {
+    const userName = prompt('What is your name?');
+    sayHello(username);
+}
+function sayHello(name) {
+    alert('Hello ' + Name + '!');
+    console.log('Alert sent!');
+}
